c0270ae65742b20cc67d5a8468ad52ddc45a9ce5
yasudamasatosi <t.taiga1112@icloud.com>
2025-07-01 15:54:39 +0900

    突起の管理方法など大幅改修

--- Assets/Scripts/ProtrusionPrefab.cs
+++ Assets/Scripts/ProtrusionPrefab.cs
@@ -6,42 +6,6 @@ public class ProtrusionPrefab : MonoBehaviour
 {
     private GameManager gameManager;
 
-    private Wall wall;
-
-    private Rigidbody2D rb;
-
-    // 移動速度
-    private float wallSpeed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // 各種変数を取得後，移動開始
-        wall = GameObject.Find("Wall").GetComponent<Wall>();
-        wallSpeed = wall.WallSpeed;
-        rb = this.GetComponent<Rigidbody2D>();
-        Move(-wallSpeed);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // 地面に達したとき，引き返す
-        if(this.transform.position.y <= -3.23f)
-            Move(wallSpeed);
-    }
-
-    private void Move(float wallSpeed){
-        // 速度をwallSpeedに
-        rb.velocity = new Vector3(0, wallSpeed, 0);
-    }
-
-/*
-    private void Stop(){
-        rb.velocity = new Vector3(0, 0, 0);
-    }
-*/
-
     // 衝突の検出
     private void OnCollisionEnter2D(Collision2D collision){
         // Playerという名前のオブジェクトに衝突した際，ゲームオーバーとする
--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -16,8 +16,6 @@ public class Wall : MonoBehaviour
 
     private Rigidbody2D rb;
 
-    private Transform tf;
-
     // 移動速度
     private float wallSpeed = 2f;
     public float WallSpeed{
@@ -43,28 +41,28 @@ public class Wall : MonoBehaviour
     // 突起を格納する配列．現時点では最大30個に設定
     private GameObject[] protrusions = new GameObject[30];
 
+    // 突起の生成位置を格納する配列
+    private float[] spawnXpositions;
+
     // 現在のレベル
     private int level = 1;
 
     // レベル更新制御用
     private int cnt = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         // 各種変数の取得
         startPos = this.transform.position;
         rb = this.GetComponent<Rigidbody2D>();
-        tf = this.GetComponent<Transform>();
+
+        InitializeSpawnPositions();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         // 位置を逐次チェックし，移動方向を制御
         if(this.transform.position.y <= -1.5f)
             Move(wallSpeed);
-        else if(this.transform.position.y > startPos.y){
+        else if(this.transform.position.y > startPos.y + 1.5f){
             Move(-wallSpeed);
             DestroyProtrusion();
             UpdateLevel();
@@ -72,27 +70,50 @@ public class Wall : MonoBehaviour
         }
     }
 
-    public void SpawnProtrusion(){
-        // 10 + 現在のレベル の数だけ突起を生成
-        for(int i = 0; i < 10 + level; ++i){
-            if(i > 30){
-                break;
-            }
+    private void InitializeSpawnPositions(){
+        spawnXpositions = new float[20];
+        float startX = -7.6f;
+        float step = 0.8f;
 
-            float randomX = Random.Range(-8.4f, 8.4f);
-            protrusions[i] = Instantiate(protrusionPrefab, this.transform.position - new Vector3(randomX, 1.75f, 0f), Quaternion.identity);
+        for (int i = 0; i < 20; i++)
+            spawnXpositions[i] = startX + i * step;
+    }
+    public void SpawnProtrusion(){
+        // 生成する突起の数
+        int spawnCount = Mathf.Min(5 + level, spawnXpositions.Length - 1);
+
+        // 重複防止のためのインデックス保存配列
+        List<int> usedIndices = new List<int>();
+
+        for (int i = 0; i < spawnCount; ++i){
+            int index;
+            int tryCount = 0;
+            // まだ突起が存在しない領域が見つかるまで繰り返し(最大100回)
+            do{
+                index = Random.Range(0, spawnXpositions.Length);
+                tryCount++;
+            } while (usedIndices.Contains(index) && tryCount < 100);
+
+            usedIndices.Add(index);
+            float x = spawnXpositions[index];
+
+            Vector3 spawnPosition = new Vector3(x, this.transform.position.y - 1.5f, 0f);
+
+            // 突起を生成
+            GameObject obj = Instantiate(protrusionPrefab, spawnPosition, Quaternion.identity, this.transform);
+            // 削除用タグを付加
+            obj.tag = "Protrusion";
+            // スケールの調整
+            obj.transform.localScale = new Vector3(0.045f, 1f, 1f);
         }
         Move(-wallSpeed);
     }
 
     private void DestroyProtrusion(){
-        // 凸部分を全て削除
-        for(int i = 0; i < 10 + level; ++i){
-            if(i > 30){
-                break;
-            }
-
-            Destroy(protrusions[i].gameObject);
+        // 子オブジェクトとして存在する突起を全て削除
+        foreach (Transform child in transform){
+            if (child.CompareTag("Protrusion"))
+                Destroy(child.gameObject);
         }
     }
 
@@ -101,18 +122,12 @@ public class Wall : MonoBehaviour
         rb.velocity = new Vector3(0, wallSpeed, 0);
     }
 
-/*
-    private void Stop(){
-        rb.velocity = new Vector3(0, 0, 0);
-    }
-*/
-
     private void UpdateLevel(){
         // 2回に1回，レベルと壁の速度が上がる
         cnt += 1;
         if(cnt == 2){
             level += cnt;
-            wallSpeed += 0.15f;
+            wallSpeed += 0.2f;
             cnt = 0;
         }
     }
