9f08c0d7d01f146f22c6ffaf58933a2e721d0682
yasudamasatosi <t.taiga1112@icloud.com>
2025-07-06 15:08:51 +0900

    プレイヤーにダッシュ機能を追加(UIは未完成)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     // プレイヤーの移動量
     [SerializeField]
-    private int moveSpeed;
+    private float moveSpeed;
+    private float dashMultiPlier = 1.5f;
+    private float dashDuration = 0.5f;
+    private float dashCoolDown = 3f;
+    private bool isDash = false;
+    private bool canDash = true;
+    private float dashCoolDownTimer = 0f;
+
+    private float keyIn;
+
     private Rigidbody2D rb;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField]
+    private Image dashCoolDownUI;
+
+    void Start(){
         rb = this.GetComponent<Rigidbody2D>();
+        if (dashCoolDownUI != null)
+            dashCoolDownUI.fillAmount = 0f;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         // 横方向のキー入力を受け付け
-        float horizontal = Input.GetAxis("Horizontal");
-        // プレイヤーに速度を与え，動かす
-        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+        keyIn = Input.GetAxis("Horizontal");
+        if (!canDash){
+            dashCoolDownTimer -= Time.deltaTime;
+            if (dashCoolDownUI != null)
+                dashCoolDownUI.fillAmount = 1 - (dashCoolDownTimer / dashCoolDown);
+
+            if (dashCoolDownTimer <= 0f){
+                canDash = true;
+                if (dashCoolDownUI != null)
+                    dashCoolDownUI.fillAmount = 0f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+            StartCoroutine(Dash());
+    }
+
+    void FixedUpdate(){
+        float currentSpped = isDash ? moveSpeed * dashMultiPlier : moveSpeed;
+        rb.velocity = new Vector2(keyIn * currentSpped, rb.velocity.y);
+    }
+
+    private IEnumerator Dash(){
+        isDash = true;
+        canDash = false;
+        dashCoolDownTimer = dashCoolDown;
+
+        if (dashCoolDownUI != null)
+            dashCoolDownUI.fillAmount = 1f;
+
+        yield return new WaitForSeconds(dashDuration);
+
+        isDash = false;
+        if (dashCoolDownUI != null)
+            dashCoolDownUI.fillAmount = 0f;
     }
 }
